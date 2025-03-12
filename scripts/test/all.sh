47fbb0eeea8bec0c2f70c58ca7cc7786c7b04ffe
Sebastian Claros <sclaros@gmail.com>
2025-03-12 12:57:21 -0300

    fix

--- scripts/test/all.sh
+++ scripts/test/all.sh
@@ -1,17 +1,22 @@
+#!/bin/bash
+script_name=$0
+script_full_path=$(dirname "$0")
+
 version=$(npx autoforce version)
 version="${version// /_}"
-./upgrade.sh
-./reset.sh
-./config.sh
-./crear-milestone.sh $version
-./crear-issue-doc.sh $version 1
-./crear-issue-dev.sh $version 2
-./crear-issue-dev.sh $version 3
-./crear-issue-dev.sh $version 4
-./start-issue.sh 1
-./create-documentation-process.sh A 1
-./finish-issue.sh 1
-./start-issue.sh 2
 
-#./start-issue.sh 2
-#./start-issue.sh 3
+${script_full_path}/upgrade.sh
+${script_full_path}/reset.sh
+${script_full_path}/config.sh
+${script_full_path}/crear-milestone.sh $version
+${script_full_path}/crear-issue-doc.sh $version 1
+${script_full_path}/crear-issue-dev.sh $version 2
+${script_full_path}/crear-issue-dev.sh $version 3
+${script_full_path}/crear-issue-dev.sh $version 4
+${script_full_path}/start-issue.sh 1
+${script_full_path}/crear-procesos.sh A 1
+${script_full_path}/finish-issue.sh 1
+${script_full_path}/start-issue.sh 2
+
+#${script_full_path}/start-issue.sh 2
+#${script_full_path}/start-issue.sh 3
